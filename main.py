bbff3272381d4c5bb2f4c5e2b3986c40c9d7da11
lhq <lanhq@datamargin.com>
2023-11-21 21:55:43 +0800

    feat:添加模型预测功能

--- main.py
+++ main.py
@@ -6,17 +6,16 @@ import torch as t
 import torch.nn as nn
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-from models.bilstm_crf_v1 import BiLSTM_CRF2 as BiLSTM_CRF
-# from models.bilstm_crf_v1 import BiLSTM_CRF3 as BiLSTM_CRF
+from models.bilstm_crf import BiLSTM_CRF
 from config import Config
 from utils.file_io import load_json
 from utils.metric import SeqEntityScore
 from utils.logger import logger, init_logger
 from utils import get_or_build_vocab, load_pretrained_embedding, DatasetLoader, ProgressBar, AverageMeter
+from utils.mix import get_entities
 
 
-
-def train_model(model, vocab, conf):
+def train(model, vocab, conf):
     train_data = load_json(conf.train_data_path)
     train_dataloader = DatasetLoader(data=train_data, 
                                      batch_size=conf.batch_size,
@@ -48,7 +47,7 @@ def train_model(model, vocab, conf):
             pbar(step=step, info={'loss': loss.item()})
             train_loss.update(loss.item(), n=1)
         train_log = {'loss': train_loss.avg}
-        eval_log, class_info = evaluate_model(model, vocab, conf)
+        eval_log, class_info = evaluate(model, vocab, conf)
         logs = dict(train_log, **eval_log)
         show_info = f'\nEpoch: {epoch} - ' + "-".join([f' {key}: {value:.4f} ' for key, value in logs.items()])
         logger.info(show_info)
@@ -65,7 +64,7 @@ def train_model(model, vocab, conf):
                 logger.info(info)
 
 
-def evaluate_model(model, vocab, conf):
+def evaluate(model, vocab, conf):
     eval_data = load_json(conf.eval_data_path)
     eval_dataloader = DatasetLoader(data=eval_data, 
                                     batch_size=conf.batch_size,
@@ -99,14 +98,38 @@ def evaluate_model(model, vocab, conf):
     return result, class_info
 
 
+def predict(model,vocab,conf):
+    # path = "checkpoints/BiLSTM_CRF_best_20231121.pth"
+    # model.load(path)
+    lines = ["四川敦煌学”。近年来，丹棱县等地一些不知名的石窟迎来了海内外的游客，他们随身携带着胡文和的著作。",
+             "即使在收藏市场上已基本绝迹，但广州货币金融博物馆拥有全套。是2000年旧馆开馆时，",
+             "原来提丽泽商务区开发好几年了，没有大的进展，原因是定位一直不好，所以定位的明确，而且坚持下去，"
+             ]
+    for line in lines:
+        words = list(line)
+        input_ids = [vocab.to_index(w) for w in words]
+        input_mask = [True] * len(words)
+        model.eval()
+        with t.no_grad():
+            input_ids = t.tensor([input_ids], dtype=t.long)
+            input_mask = t.tensor([input_mask], dtype=t.bool)
+            tags = model.forward_tags(input_ids, input_mask)
+        label_entities = get_entities(tags[0], conf.id2label)
+        items = []
+        for tag, start, end in label_entities:
+            words = line[start:end+1]
+            item = dict(tag=tag,start=start,end=end,entity=words)
+            items.append(item)
+        # print(line)
+        print(items) 
+
 
 @click.command()
-@click.option('--train', is_flag=True, help='模型训练')
-@click.option('--eval', is_flag=True, help="模型验证")
+@click.option('-t', '--task', type=click.Choice(['train', 'eval','predict']),default='train', help='任务')
 @click.option('--pretrained', is_flag=True, help='使用预训练词向量')
 @click.option('-m', "--model", type=click.Choice(['bilstm_crf']), default='bilstm_crf', help="模型", show_default=True)
 @click.option('--gpu', type=int, default=None, help='GPU')
-def main(train,eval, model, gpu, pretrained):
+def main(task, model, gpu, pretrained):
     if isinstance(gpu, int):
         device = t.device(f"cuda:{gpu}")
     else:
@@ -122,24 +145,33 @@ def main(train,eval, model, gpu, pretrained):
     else:
         vocab = get_or_build_vocab(conf)
 
-    ner_model = BiLSTM_CRF(vocab_size=len(vocab),
-                       embedding_size=conf.embeding_size,
-                       hidden_size=conf.hidden_size,
-                       label_size=len(conf.label2id)
-                       ) 
+    if task == "train":
+        # ner_model = BiLSTM_CRF(vocab_size=len(vocab),
+        #                        embedding_size=conf.embeding_size,
+        #                        hidden_size=conf.hidden_size,
+        #                        label_size=len(conf.label2id)
+        #                        )
+        ner_model = BiLSTM_CRF(len(vocab),
+                               embedding_size=conf.embeding_size,
+                               hidden_size=conf.hidden_size,
+                               label_size=len(conf.label2id)
+                               )
     
-    if pretrained:
-        # 加载预训练词向量
-        ner_model.load_pre_train_embedding(embedding)
+        if pretrained:
+            # 加载预训练词向量
+            ner_model.load_pre_train_embedding(embedding)
 
-    ner_model.to(device)
 
-    if train:
         log_path = os.path.join(conf.cache_dir, f'{model}-train.log')
         init_logger(log_file=log_path)
-        train_model(ner_model, vocab, conf)
-    if eval:
-        pass 
+        # ner_model.to(device)
+        train(ner_model, vocab, conf)
+
+    if task == "eval":
+        pass
+    if task == "predict":
+        ner_model, _ = BiLSTM_CRF.load_model("checkpoints/BiLSTM_CRF_best_20231121_old.pth")
+        predict(ner_model, vocab, conf)
 
 
 
