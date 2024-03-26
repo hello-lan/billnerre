e94fc0b9314702fb32d1902fb6a881692a8f77bc
lhq <215224901@qq.com>
2024-03-26 10:39:55 +0800

    refactor: 重构api测试数据生成脚本

--- scripts/create_api_test_message.py
+++ scripts/create_api_test_message.py
@@ -3,7 +3,18 @@ import random
 import re
 
 
-def read_and_convert_msg(path, roomName):
+def load_json(path):
+    with open(path,'r') as f:
+        data = json.load(f)
+    return data
+
+
+def save_json(data, path):
+    with open(path,'w') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+
+def read_raw_msg(path, roomName):
     # 本地读取数据
     with open(path) as f:
         txt = f.read()
@@ -32,14 +43,26 @@ def read_and_convert_msg(path, roomName):
     return data
 
 
-if __name__ == "__main__":
+def create_rela_data():
     path1 = "data/origin_data/多多碰票群_202311081940.txt"
-    tmp1 = read_and_convert_msg(path1,roomName="多多碰票群")
+    tmp1 = read_raw_msg(path1,roomName="多多碰票群")
     path2 = "data/origin_data/惠鑫兑银行交流群_202311081949.txt"
-    tmp2 = read_and_convert_msg(path1,roomName="惠鑫兑银行交流群")
+    tmp2 = read_raw_msg(path1,roomName="惠鑫兑银行交流群")
 
     tmp = tmp1+tmp2
     sample = random.sample(tmp, 1000)
+    save_json(sample, "output/test_rela_api_data.json")   
 
-    with open("output/test_msg.json","w") as f:
-        json.dump(sample,f, indent=2, ensure_ascii=False)
+
+def create_ner_data():
+    data = load_json("../billnerre/data/corpus_msg/dev_processed.json")
+    tmp = []
+    for item in data:
+        new_item = dict(post_data=dict(content=item["text"]),response=dict(preds=item["tag"].split()))
+        tmp.append(new_item)
+    save_json(tmp, "output/test_ner_api_data.json")
+
+
+if __name__ == "__main__":
+    create_rela_data()
+    create_ner_data()
